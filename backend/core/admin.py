86e083a54000a52056ebabdf75d305cad99938f1
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-09-19 22:09:43 +0200

    Register models to Django admin panel

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from .models import Company, Department, Employee, Role
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("company_name", "company_registration_number", "company_address")
+    list_filter = ("company_name", "company_registration_number")
+    search_fields = ("company_name", "company_registration_number")
+    ordering = ("company_name", "company_registration_number")
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("department_name", "company")
+    list_filter = ("department_name", "company")
+    search_fields = ("department_name", "company")
+    ordering = ("department_name", "company")
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("role_name", "department")
+    list_filter = ("role_name", "department")
+    search_fields = ("role_name", "department")
+    ordering = ("role_name", "department")
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        "employee_name",
+        "employee_id",
+        "date_started",
+        "date_left",
+        "role_name",
+    )
+    list_filter = (
+        "employee_name",
+        "employee_id",
+        "date_started",
+        "date_left",
+        "role_name",
+    )
+    search_fields = (
+        "employee_name",
+        "employee_id",
+        "date_started",
+        "date_left",
+        "role_name",
+    )
+    ordering = (
+        "employee_name",
+        "employee_id",
+        "date_started",
+        "date_left",
+        "role_name",
+    )
