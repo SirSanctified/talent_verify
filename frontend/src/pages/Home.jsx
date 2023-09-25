605ebd40464c12b3642ac48e1a5ca15870da89b8
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-09-25 12:16:37 +0200

    Create  home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Stats from "../components/Stats";
+import Advantage from "../components/Advantage";
+import CTA from "../components/CTA";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Stats />
+      <Advantage />
+      <CTA />
+    </>
+  );
+}
